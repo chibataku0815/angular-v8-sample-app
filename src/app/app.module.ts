0e7c7dfc6140df5960e3d141f5add788a38ceecc
chibataku0815 <chibataku0815@gmail.com>
2019-12-16 17:02:15 +0900

    Changed rule and fix for semi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { ProductListComponent } from './product-list/product-list.component'
 
 @NgModule({
   declarations: [
